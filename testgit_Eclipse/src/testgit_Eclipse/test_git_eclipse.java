8cb65c77c28134bbdc68145335e4745da3a5fc97
bae <seongbin5962@daum.net>
2020-12-23 23:45:27 +0900

    gittest

---
+++ testgit_Eclipse/src/testgit_Eclipse/test_git_eclipse.java
@@ -0,0 +1,10 @@
+package testgit_Eclipse;
+
+public class test_git_eclipse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("git_test");
+	}
+
+}
